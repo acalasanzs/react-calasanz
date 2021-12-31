eff516266229b1881fff8cf901e55330d01c7eff
ACS-PSI <acspsi@users.noreply.github.com>
2021-12-31 23:48:22 +0100

    Doing Awesome Stuff Here

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Viatja compartint',
+    headline: "Publica el teu pròxim viatge d'anada i volta amb Calasanz... ",
+    description: "i porta't, de mitjana, 60€* dels teus passatgers. Reservar un viatge és més fàcil que mai! Només necessitaràs un parell de minuts per a publicar el teu trajecte. Compartim viatge?",
+    buttonLabel: 'Comença ara',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Cotxe',
+    dark: true,
+    primary: true,
+    darkText: false
+}
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import { Button } from 'react-scroll'
+import { InfoContainer, InfoRow, InfoWrapper, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap,ImgWrap, Img} from './InfoElements'
 
 const InfoSection = () => {
     return (
         <>
-          <InfoContainer id={id}>
+          <InfoContainer>
             <InfoWrapper>
               <InfoRow>
                 <Column1>
-                <TextWrapper></TextWrapper>
+                <TextWrapper>
+                  <TopLine>TopLine</TopLine>
+                  <Heading>Heading</Heading>
+                  <Subtitle>Subtitle</Subtitle>
+                  <BtnWrap><Button to="home">Button</Button></BtnWrap>
+                </TextWrapper>
                 </Column1>
+                <Column2>
+                  <ImgWrap>
+                    <Img />
+                  </ImgWrap>
+                </Column2>
               </InfoRow>
             </InfoWrapper>
           </InfoContainer>
