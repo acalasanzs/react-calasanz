267fbbc2da4834c5909cc4f899a2ac0f38123f93
ACS-PSI <acspsi@users.noreply.github.com>
2021-12-31 20:16:44 +0100

    NC

---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const InfoSection = () => {
+    return (
+        <>
+          <InfoContainer id={id}>
+            <InfoWrapper>
+              <InfoRow>
+                <Column1>
+                <TextWrapper></TextWrapper>
+                </Column1>
+              </InfoRow>
+            </InfoWrapper>
+          </InfoContainer>
+        </>
+    )
+}
+
+export default InfoSection
